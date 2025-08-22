99ffa243776eeb11b2c96ade6fc6c6762a56cf56
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-08-22 11:24:32 -0300

    fix: folder reestructure

---
+++ frontend/src/services/recommendation/helpers/index.js
@@ -0,0 +1,8 @@
+import normalize from './normalize';
+import tokens from './tokens';
+import extractStringsArray from './extractStringsArray';
+
+export { tokens, normalize, extractStringsArray };
+// export { scoreForEntry } from './scoreForEntry';
+// export { buildProductEntries } from './buildProductEntries';
+// export { explainMatches } from './explainMatches';
