c3e1d6ab20de2ffee5abee6aa684eb994c7f0fa9
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-08-22 18:29:25 -0300

    feat: recommendations list styles

--- frontend/src/components/RecommendationList/RecommendationList.js
+++ frontend/src/components/RecommendationList/RecommendationList.js
@@ -1,18 +1,36 @@
-import React from 'react';
+const recomendationsStyles = {
+  base: 'py-3',
+  repeatable: 'border-b-2 ',
+};
 
 function RecommendationList({ recommendations }) {
   return (
-    <div>
-      <h2 className="text-lg font-bold mb-4">Lista de Recomendações:</h2>
-
+    <div className="p-4 rounded-lg shadow-md">
+      <h2 className="text-lg font-bold mb-2">Lista de Recomendações:</h2>
       {recommendations.length === 0 && <p>Nenhuma recomendação encontrada.</p>}
 
       <ul>
-        {recommendations.map((recommendation, index) => (
-          <li key={index} className="mb-2">
-            {recommendation.name}
-          </li>
-        ))}
+        {recommendations.map((recommendation, index) => {
+          const styles = [
+            recomendationsStyles.base,
+            index < recommendations.length - 1
+              ? recomendationsStyles.repeatable
+              : '',
+          ].join(' ');
+
+          return (
+            <li key={index} className={styles}>
+              <h3 className="mb-2 font-semibold">{recommendation.name}</h3>
+              <ul>
+                {recommendation.features.map((feature, idx) => (
+                  <li key={idx} className="">
+                    {feature}
+                  </li>
+                ))}
+              </ul>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
