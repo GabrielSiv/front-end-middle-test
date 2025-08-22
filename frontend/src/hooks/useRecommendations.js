96d7b174208a5f475ccdad5166ad1bb940a596fc
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-08-22 11:42:53 -0300

    feat: imports update

--- frontend/src/hooks/useRecommendations.js
+++ frontend/src/hooks/useRecommendations.js
@@ -1,13 +1,13 @@
 // useRecommendations.js
 
 import { useState } from 'react';
-import recommendationService from '../services/recommendation.service';
+import { default as getRecommendationsService } from '../services/recommendation';
 
 function useRecommendations(products) {
   const [recommendations, setRecommendations] = useState([]);
 
   const getRecommendations = (formData) => {
-    return recommendationService.getRecommendations(formData, products);
+    return getRecommendationsService(formData, products);
   };
 
   return { recommendations, getRecommendations, setRecommendations };
